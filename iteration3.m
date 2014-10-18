9275f6f2b988a068051ac62bde443ebd995a92d3
lindsey-andrade <lindsey.andrade@students.olin.edu>
2014-10-18 15:23:44 -0400

    fixed It3 minor errors

--- iteration3.m
+++ iteration3.m
@@ -2,12 +2,14 @@ function iteration3()
 
 P = 1000; 
 
+stress([pi/2, 30, 50, 80])
+
 
     function [c, ceq] = stress(X)
-        theta = stress(1); 
-        a = stress(2); 
-        b = stress(3); 
-        c = stress(4); 
+        theta = X(1); 
+        a = X(2); 
+        b = X(3); 
+        c = X(4); 
         
         z = sin(theta)*(a + (c-a)/2);
         
@@ -25,9 +27,9 @@ P = 1000;
         
         Am = b*log(c/a);
         
-        tStress = N/A + M(A - a*Am)/(A*a(R*Am - A));
+        tStress = N/A + M*(A - a*Am)/(A*a*(R*Am - A));
         
-        cStress = N/A + M(A - c*Am)/(A*c(R*Am - A)); 
+        cStress = N/A + M*(A - c*Am)/(A*c*(R*Am - A)); 
         
         totalStress = tStress + cStress;  
         
